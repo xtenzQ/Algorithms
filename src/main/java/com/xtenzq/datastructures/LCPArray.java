0f14121242b6d452858e039b853bbfe7370ce054
Nikita Rusetskii <rusetscky@outlook.com>
2023-03-25 18:42:00 +0700

    add comments and tests for LCP array

--- src/main/java/com/xtenzq/datastructures/LCPArray.java
+++ src/main/java/com/xtenzq/datastructures/LCPArray.java
@@ -3,30 +3,59 @@ package com.xtenzq.datastructures;
 import com.xtenzq.datastructures.utils.Entry;
 import com.xtenzq.datastructures.utils.GenericArray;
 
+/**
+ * The LCP array is an array in which every index tracks how many characters two sorted adjacent suffixes have in common
+ * @author Nikita Rusetskii
+ */
 public class LCPArray extends GenericArray<LCPArray.LCPEntry> {
+
+    private final SuffixArray suffixArray;
 
     public LCPArray(String text) {
         super(LCPEntry.class, text.length());
-        SuffixArray suffixArray = new SuffixArray(text);
-        array = new LCPEntry[suffixArray.getSize()];
-        build(suffixArray);
+        suffixArray = new SuffixArray(text);
+        build();
     }
 
-    private void build(SuffixArray suffixArray) {
+    /**
+     * Constructs an LCP array by counting common substring chars at the beginning of suffix
+     * Time complexity: O(n)
+     */
+    private void build() {
         String previous = "";
         int index = 0;
-        for (SuffixArray.SuffixEntry entry : suffixArray) {
+        for (SuffixArray.Suffix entry : suffixArray) {
             array[index] = new LCPEntry(getCommonSubstring(previous, entry.getValue()), entry);
             previous = entry.getValue();
             index++;
         }
     }
 
-    private int getCommonSubstring(String str1, String str2) {
+    /**
+     * Returns an array of the LCP values contained in this array
+     * @return array of LCP values
+     */
+    public int[] valueList() {
+        int[] lcpValues = new int[getSize()];
+        for (int i = 0; i < getSize(); i++) {
+            lcpValues[i] = array[i].getIndex();
+        }
+        return lcpValues;
+    }
+
+    /**
+     * Get common substring at the beginning of the string
+     * Time complexity: O(n)
+     *
+     * @param firstString first string
+     * @param secondString second string
+     * @return number of common characters
+     */
+    private int getCommonSubstring(String firstString, String secondString) {
         int sub = 0;
-        int minSize = Math.min(str1.length(), str2.length());
+        int minSize = Math.min(firstString.length(), secondString.length());
         for (int i = 0; i < minSize; i++) {
-            if (str1.charAt(i) == str2.charAt(i)) {
+            if (firstString.charAt(i) == secondString.charAt(i)) {
                 sub++;
             }
             else {
@@ -36,10 +65,10 @@ public class LCPArray extends GenericArray<LCPArray.LCPEntry> {
         return sub;
     }
 
-    static class LCPEntry extends Entry<Integer, SuffixArray.SuffixEntry> {
+    protected static class LCPEntry extends Entry<Integer, SuffixArray.Suffix> {
 
-        public LCPEntry(int value, SuffixArray.SuffixEntry suffixEntry) {
-            super(value, suffixEntry);
+        public LCPEntry(int value, SuffixArray.Suffix suffix) {
+            super(value, suffix);
         }
     }
 }
---
+++ src/test/java/com/xtenzq/datastructures/LCPArrayTest.java
@@ -0,0 +1,17 @@
+package com.xtenzq.datastructures;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LCPArrayTest {
+
+    @Test
+    public void testLCPArray() {
+        LCPArray lcpArray = new LCPArray("ABABBAB");
+        int[] actual = lcpArray.valueList();
+        int[] expected = new int[] {0, 2, 2, 0, 1, 3, 1};
+        assertArrayEquals(expected, actual);
+    }
+
+}
