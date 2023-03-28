b0ffab3f5452a8d3359a3349d6b7bb46057be859
Nikita Rusetskii <rusetscky@outlook.com>
2023-03-28 14:34:02 +0700

    fix comments in Tree.java

--- src/main/java/com/xtenzq/datastructures/utils/Tree.java
+++ src/main/java/com/xtenzq/datastructures/utils/Tree.java
@@ -1,52 +1,52 @@
 package com.xtenzq.datastructures.utils;
 
 /**
- * Tree is an abstract data type that represents hierarchical tree structure with a set of connected nodes.
+ * An abstract data type representing a hierarchical tree structure consisting of connected nodes.
  *
- * @param <T> the type of elements in this tree
+ * @param <T> the type of elements stored in this tree
+ * @since 27.03.2023
  * @author Nikita Rusetskii
  */
 public interface Tree<T> {
 
     /**
-     * Adds the specified value to the tree.
+     * Inserts the specified value into the tree.
      *
-     * @param value value to be added to this tree
-     * @return {@code true} if this tree changed as a result of the call
+     * @param value the value to be inserted into this tree
+     * @return {@code true} if this tree changed as a result of the insertion
      * @throws ClassCastException if the class of the specified element
-     *         prevents it from being added to this tree
+     *         prevents it from being inserted into this tree
      * @throws NullPointerException if the specified element is null and this
      *         tree does not permit null elements
      * @throws IllegalArgumentException if some property of this element
-     *         prevents it from being added to this tree
+     *         prevents it from being inserted into this tree
      */
     boolean insert(T value);
 
     /**
-     * Removes the node from this tree if it is present (optional operation).
-     * More formally, if this tree contains a node with value {@code value},
-     * that node is removed.
+     * Removes the node with the specified value from this tree (optional operation).
+     * If this tree contains a node with the value {@code value}, that node is removed.
      *
-     * @param value value to be removed from the tree
+     * @param value the value to be removed from this tree
      * @throws UnsupportedOperationException if the {@code remove} operation
      *         is not supported by this tree
      * @throws ClassCastException if the value is of an inappropriate type for
-     *         this map
+     *         this tree
      * @throws NullPointerException if the specified value is null and this
      *         tree does not permit null nodes
      */
     void remove(T value);
 
     /**
-     * Removes all of the elements from this list. The list will be empty after this call returns.
+     * Removes all nodes from this tree. The tree will be empty after this call returns.
      */
     void clear();
 
     /**
      * Returns {@code true} if this tree contains the specified value.
      *
-     * @param value value whose presence in this tree is to be tested
-     * @return {@code true} if this tree contains one or more nodes that maps to the
+     * @param value the value whose presence in this tree is to be tested
+     * @return {@code true} if this tree contains one or more nodes with the
      *         specified value
      * @throws ClassCastException if the value is of an inappropriate type for
      *         this tree
@@ -56,9 +56,8 @@ public interface Tree<T> {
     boolean contains(T value);
 
     /**
-     * Returns the number of nodes in this tree.  If the
-     * tree contains more than {@code Integer.MAX_VALUE} elements, returns
-     * {@code Integer.MAX_VALUE}.
+     * Returns the number of nodes in this tree. If the tree contains more than
+     * {@code Integer.MAX_VALUE} nodes, returns {@code Integer.MAX_VALUE}.
      *
      * @return the number of nodes in this tree
      */
